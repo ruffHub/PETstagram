6219ac83067c7d0915b2563aeae8720ecb9166fe
Ruslan Smolovik <googruff@gmail.com>
2017-05-18 19:11:29 +0300

    test commit

--- app/app.js
+++ app/app.js
@@ -1,27 +1,28 @@
 (function() {
 'use strict';
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.login',
-  'myApp.main',
-  'myApp.profile'
-])
+angular
+    .module('myApp', [
+        'ngRoute',
+        'myApp.login',
+        'myApp.main',
+        'myApp.profile'
+    ])
 
-.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-  $routeProvider.otherwise({redirectTo: '/main'});
-}])
+    .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+      $locationProvider.hashPrefix('!');
+      $routeProvider.otherwise({redirectTo: '/main'});
+    }])
 
-.service('mainService', ['$http', function ($http) {
-    return {
-        getPosts: function () {
-            return $http.get('https://jsonplaceholder.typicode.com/photos/');
-        },
-        getCurrentPost: function (i) {
-            return $http.get('https://jsonplaceholder.typicode.com/photos/' + i);
-        }
-    };
-}]);
+    .service('mainService', ['$http', function ($http) {
+        return {
+            getPosts: function () {
+                return $http.get('https://jsonplaceholder.typicode.com/photos/');
+            },
+            getCurrentPost: function (i) {
+                return $http.get('https://jsonplaceholder.typicode.com/photos/' + i);
+            }
+        };
+    }]);
 
 })();
--- app/views/main/main.js
+++ app/views/main/main.js
@@ -1,43 +1,44 @@
 (function() {
 'use strict';
 
-angular.module('myApp.main', ['ngRoute'])
+angular
+    .module('myApp.main', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-      .when('/main', {
-          resolve: {
-              "check": function ($rootScope, $location) {
-                      if(!$rootScope.loggedIn) {
-                          $location.path('/login');
+    .config(['$routeProvider', function($routeProvider) {
+      $routeProvider
+          .when('/main', {
+              resolve: {
+                  "check": function ($rootScope, $location) {
+                          if(!$rootScope.loggedIn) {
+                              $location.path('/login');
+                          }
                       }
-                  }
-              },
-          templateUrl: 'views/main/main.html',
-          controller: 'mainCtrl',
-          controllerAs: 'main'
-      })
-      .when('/post', {
-          templateUrl: 'views/main/post.html',
-          controller: 'mainCtrl',
-          controllerAs: 'main'
-      });
-}])
+                  },
+              templateUrl: 'views/main/main.html',
+              controller: 'mainCtrl',
+              controllerAs: 'main'
+          })
+          .when('/post', {
+              templateUrl: 'views/main/post.html',
+              controller: 'mainCtrl',
+              controllerAs: 'main'
+          });
+    }])
 
-.controller('mainCtrl', ['$location', '$routeParams', 'mainService', function($location ,$routeParams, mainService) {
-    var self = this;
+    .controller('mainCtrl', ['$location', '$routeParams', 'mainService', function($location ,$routeParams, mainService) {
+        var self = this;
 
-    mainService.getPosts().then(function (response) {
-        self.posts = response.data;
-        console.log(self.posts);
-    });
+        mainService.getPosts().then(function (response) {
+            self.posts = response.data;
+            console.log(self.posts);
+        });
 
-    this.postsCount = 15;
-    this.showMorePosts = function () {
-        self.postsCount += 15;
-    };
+        this.postsCount = 15;
+        this.showMorePosts = function () {
+            self.postsCount += 15;
+        };
 
-    this.postId = $routeParams.postId;
-}]);
+        this.postId = $routeParams.postId;
+    }]);
 
 })();
